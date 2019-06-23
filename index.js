3c2950d7e68b6dc41e23db21aa5fafc109295bc7
AlexanderMac <amatsibarov@gmail.com>
2019-06-23 21:37:22 +0300

    fix typing error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.parse = require('./src/parsers');
-exports.build = require('./src/builders');
-exports.conts = require('./src/consts');
+exports.parse  = require('./src/parsers');
+exports.build  = require('./src/builders');
+exports.consts = require('./src/consts');
