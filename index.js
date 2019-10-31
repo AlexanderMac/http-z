88ee8c436db28f68f5f8e3aecf38c3c9e3be760a
AlexanderMac <amatsibarov@gmail.com>
2019-10-31 22:39:56 +0300

    export utils

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 exports.parse      = require('./src/parsers');
 exports.build      = require('./src/builders');
 exports.consts     = require('./src/consts');
+exports.utils      = require('./src/utils');
 exports.HttpZError = require('./src/error');
