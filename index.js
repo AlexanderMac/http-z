446f146a73a812961a04b027619bedab3ef9dee0
AlexanderMac <amatsibarov@gmail.com>
2019-10-31 22:49:55 +0300

    remove utils export

--- index.js
+++ index.js
@@ -3,5 +3,4 @@
 exports.parse      = require('./src/parsers');
 exports.build      = require('./src/builders');
 exports.consts     = require('./src/consts');
-exports.utils      = require('./src/utils');
 exports.HttpZError = require('./src/error');
