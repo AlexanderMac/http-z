859ce8e9de6a7599fd3f898e7e8e197a7e4b2bc9
alexmac <amatsibarov@gmail.com>
2020-10-19 17:15:09 +0300

    integrate rollup

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+const commonjs = require('@rollup/plugin-commonjs')
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const pkg = require('./package.json')
+
+module.exports = {
+  input: 'index.js',
+  output: {
+    file: pkg.module,
+    name: 'httpZ',
+    format: 'umd',
+    globals: {
+      'lodash': '_',
+      'qs': 'qs'
+    }
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs()
+  ],
+  external: [
+    'lodash',
+    'qs'
+  ]
+}
